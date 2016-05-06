a655b0c16ed37c86d8937c55c813192916ae5a00
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-05-06 15:41:12 -0500

    Finished all where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,4 +4,6 @@
 
 /* SELECT * FROM employees WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31' */
 
-SELECT * FROM employees WHERE birth_date LIKE '%-12-25'
+/* SELECT * FROM employees WHERE birth_date LIKE '%-12-25' */
+
+SELECT * FROM employees WHERE last_name LIKE '%q%'
