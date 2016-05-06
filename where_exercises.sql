5b90f92e042febbda58fc5f63e8fd477cd48c98c
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-05-06 15:38:16 -0500

    finished third portion of where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,5 @@
 /* SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya'); */
 
-SELECT * FROM employees WHERE last_name like 'E%'
+-- SELECT * FROM employees WHERE last_name like 'E%'
 
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31'
