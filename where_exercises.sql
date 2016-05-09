a458d284315466174a229bb540e076b0d9c83ebf
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-05-09 11:10:48 -0500

    Fixed this

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,8 +10,8 @@ OR last_name like '%E';
 SELECT * FROM employees WHERE last_name like 'E%'
 AND last_name like '%E';
 
-/* SELECT * FROM employees WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31'; */
+SELECT * FROM employees WHERE birth_date LIKE '%-12-25'
+AND hire_date BETWEEN '1990-1-1' AND '1999-12-31';
 
-/* SELECT * FROM employees WHERE birth_date LIKE '%-12-25'; */
-
-/* SELECT * FROM employees WHERE last_name LIKE '%q%'; */
+SELECT * FROM employees WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
